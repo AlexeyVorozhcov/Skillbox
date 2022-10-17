b97ce4012f9d8ac3090ef697f77b579be68fec46
AlexeyVorozhcov <alexey.vorozhcov@gmail.com>
2022-10-17 22:23:28 +0300

    end_of_1day

--- main.py
+++ main.py
@@ -3,11 +3,13 @@
 import logging
 from aiogram import Bot, Dispatcher, executor, types
 from config_reader import config
-# from settings_secret import API_TOKEN
 
 # Включаем логирование, чтобы не пропустить важные сообщения
 logging.basicConfig(level=logging.INFO)
 
+# Для записей с типом Secret* необходимо
+# вызывать метод get_secret_value(),
+# чтобы получить настоящее содержимое вместо '*******'
 API_TOKEN = config.bot_token.get_secret_value()
 
 bot = Bot(token=API_TOKEN)
@@ -25,6 +27,12 @@ async def send_welcome(message: types.Message):
 # Создаём функцию с простой задачей — отправить обратно тот же текст, что ввёл пользователь.
 async def echo(message: types.Message):
    await message.answer(message.text)
+
+
+@dp.message(commands=["test"])
+async def any_message(message: types.Message):
+    await message.answer("Hello, <b>world</b>!", parse_mode="HTML")
+    await message.answer("Hello, *world*\!", parse_mode="MarkdownV2")
 
 
 if __name__ == '__main__':
